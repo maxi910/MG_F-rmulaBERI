b6ce43111a0e71d76e8273ac06d7b8263b471ffe
maxi910 <145173412+maxi910@users.noreply.github.com>
2025-12-10 19:21:24 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('cont-cache-v3-8').then((cache) => cache.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((resp) => resp || fetch(e.request)));
+});
