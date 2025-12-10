c2c4ec1f4eca2976b1dddb4f60ff70fb84f1841e
maxi910 <145173412+maxi910@users.noreply.github.com>
2025-12-10 21:03:16 +0100

    Update service worker cache version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('cont-cache-v3-8').then((cache) => cache.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
+  e.waitUntil(caches.open('cont-cache-v38').then((cache) => cache.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((resp) => resp || fetch(e.request)));
