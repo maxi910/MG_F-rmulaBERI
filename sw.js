8db026033e01ae68b73400a0c8ae2f75984e8889
maxi910 <145173412+maxi910@users.noreply.github.com>
2025-12-10 21:08:24 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,52 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('cont-cache-v38').then((cache) => cache.addAll(['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'])));
+const CACHE_NAME = 'cont-cache-v39';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Se instala el SW y se precachean assets básicos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((resp) => resp || fetch(e.request)));
+
+// Limpiar caches viejas cuando se activa el nuevo SW
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia de fetch
+self.addEventListener('fetch', event => {
+  // Para navegaciones (HTML): network-first
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+    return;
+  }
+
+  // Para el resto: cache-first
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
